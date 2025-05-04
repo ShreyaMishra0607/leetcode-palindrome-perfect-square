d0a251de59f486df962132973f3322acf6390350
ShreyaMishra0607 <mishrashreya0607@gmail.com>
2025-05-04 16:01:00 +0530

    Create palindromeLinkedList.cpp

---
+++ palindromeLinkedList.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        // Step 1: Find the middle of the list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse the second half of the list
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // Step 3: Compare first and second half
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = prev;  // head of reversed second half
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val) return false;
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        return true;
+    }
+};
